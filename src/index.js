963e636a61c400a449d29aab94688ad469e03fa2
Brazy <me@brazy.one>
2022-08-17 18:39:28 +0300

    Add notes

--- src/index.js
+++ src/index.js
@@ -1,58 +1,90 @@
-const folders = ['Hot', 'Archive'];
+const Run = (() => { 
 
-const showFolders = () => {
-    let content = document.getElementById('content')
-    content.innerHTML = ''
-    folders.forEach((item) => {
-        let div = document.createElement('div');
-        div.className = 'folder'
-        div.innerText = item;
-        content.appendChild(div);
-        let remove = document.createElement('button');
-        remove.className = 'remove';
-        remove.innerText = '-';
-        div.appendChild(remove);
-        let add = document.createElement('button');
-        add.className = 'add';
-        add.innerText = '+';
-        div.appendChild(add);
-        let collapse = document.createElement('button');
-        collapse.className = 'collapse';
-        collapse.innerText = '⌵';
-        div.appendChild(collapse);
-    })
-}
-
-showFolders();
-
-const addFolder = () => {
-    let name = prompt('Name:')
-    folders.push(name);
-    showFolders();
-    removeFolder();
-}
-
-document.getElementById('folder').addEventListener(('click'), () => {
-    addFolder();
-});
-
-const removeFolder = () => {
-    for (let i = 0; i < folders.length; i++) {
-        document.querySelectorAll('.remove')[i].addEventListener('click', () => {
-            folders.splice(i, 1);
-            showFolders(); 
-            removeFolder();
-        })
+    const app = () => {
+        showFolders();
+        removeFolder();
     }
-}
 
-removeFolder();
+    const notes = [
+        {
+            'name': 'Shopping list',
+            'description': 'Milk, Eggs, Apples'
+        }
+    ]
 
-const createNote = (tit, desc) => {
+    const folders = [
+        {
+            'name': 'ASAP',
+            notes
+        },
+        {
+            'name': 'HOT'
+        },
+        {
+            'name': 'Books'
+        }
+    ];
+
+    const showFolders = () => {
+        let content = document.getElementById('content')
+        content.innerHTML = '';
+        for (let i = 0; i < folders.length; i++) {
+            let div = document.createElement('div');
+            div.className = 'folder'
+            div.innerText = folders[i].name;
+            content.appendChild(div);
+            let remove = document.createElement('button');
+            remove.className = 'remove';
+            remove.innerText = '-';
+            div.appendChild(remove);
+            let add = document.createElement('button');
+            add.className = 'add';
+            add.innerText = '+';
+            div.appendChild(add);
+            let collapse = document.createElement('button');
+            collapse.className = 'collapse';
+            collapse.innerText = '⌵';
+            div.appendChild(collapse);
+            if (folders[i].hasOwnProperty('notes')) {
+                let note = document.createElement('div');
+                note.className = 'note';
+                note.innerText = folders[i].notes[0].name;
+                content.appendChild(note);
+            }
+        };
+    }
+
+    const createFolder = (name) => {
+        return {
+            'name': name
+        }
+    }
+
+    const addFolder = () => {
+        let name = prompt('Name:');
+        folders.push(createFolder(name));
+        showFolders();
+        removeFolder();
+    }
+
+    document.getElementById('folder').addEventListener(('click'), () => {
+        addFolder();
+    });
+
+    const removeFolder = () => {
+        for (let i = 0; i < folders.length; i++) {
+            document.querySelectorAll('.remove')[i].addEventListener('click', () => {
+                folders.splice(i, 1);
+                showFolders(); 
+                removeFolder();
+            })
+        }
+    }
+
     return {
-        title: tit,
-        description: desc,
+        app
     }
-}
 
-let note1 = createNote('ASAP', 'Buy milk');
+})();
+
+Run.app();
