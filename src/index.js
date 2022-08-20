8c03f94f647faa3d70473ade8831812449daa80a
Brazy <me@brazy.one>
2022-08-20 16:37:44 +0300

    Remove note 2/2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const Run = (() => {
 
     const app = () => {
         showFolders();
-        removeFolder();
     }
 
     const notes = [
@@ -27,7 +26,10 @@ const Run = (() => {
             },
         ],
         [
-
+            {
+                'name': 'Hey',
+                'description': 'Bye'
+            },
         ],
     ];
 
@@ -50,7 +52,6 @@ const Run = (() => {
         let content = document.getElementById('content')
         content.innerHTML = '';
         for (let i = 0; i < folders.length; i++) {
-            
             let div = document.createElement('div');
             div.className = 'folder';
             div.innerText = folders[i].name;
@@ -60,25 +61,40 @@ const Run = (() => {
             remove.className = 'remove';
             remove.innerText = '-';
             div.appendChild(remove);
+
+            remove.addEventListener('click', () => {
+                folders.splice(i, 1);
+                notes.splice(i, 1);
+                showFolders();
+            });
             
             let add = document.createElement('button');
             add.className = 'add';
             add.innerText = '+';
             div.appendChild(add);
+
+            add.addEventListener('click', () => {
+                let name = prompt('Name:');
+                notes[i].push(createNote(name));
+                showFolders();
+            });
             
             let collapse = document.createElement('button');
             collapse.className = 'collapse';
             collapse.innerText = '⌵';
             div.appendChild(collapse);
-            
-            let rightNote = document.getElementById('note');
+        }
+        showNotes();
+    }
 
-            for (const value of folders[i].notes) {
-
+    const showNotes = () => {
+        for (let i = 0; i < folders.length; i++) {
+            for (let j = 0; j < folders[i].notes.length; j++) {
                 let note = document.createElement('div');
                 note.className = 'note';
-                note.innerText = value.name;
-                content.appendChild(note);
+                note.innerText = folders[i].notes[j].name;
+                let currentFolder = document.getElementsByClassName('folder')[i];
+                currentFolder.appendChild(note);
 
                 let removenote = document.createElement('button');
                 removenote.className = 'removenote';
@@ -86,23 +102,25 @@ const Run = (() => {
                 note.appendChild(removenote);
 
                 removenote.addEventListener('click', () => {
-                    notes[i].splice(1, 1);
+                    notes[i].splice(j, 1);
                     showFolders();
-                });
-
-                let textarea = document.createElement('textarea');
-                note.addEventListener('click', () => {
-                    rightNote.innerHTML = '';
-                    textarea.value = value.description;
-                    rightNote.appendChild(textarea);
-                    textarea.focus();
-                });
-                
-                textarea.addEventListener('keyup', () => {
-                    value.description = textarea.value;
                 });
             }
         }
+    }
+
+    const noteText = () => {
+        let rightNote = document.getElementById('note');
+        let textarea = document.createElement('textarea');
+        note.addEventListener('click', () => {
+            rightNote.innerHTML = '';
+            textarea.value = folders[i].notes[j].description;
+            rightNote.appendChild(textarea);
+            textarea.focus();
+        });
+        textarea.addEventListener('keyup', () => {
+            folders[i].notes[j].description = textarea.value;
+        });
     }
 
     const createFolder = (name) => {
@@ -116,21 +134,17 @@ const Run = (() => {
         let name = prompt('Name:');
         folders.push(createFolder(name));
         showFolders();
-        removeFolder();
     }
 
     document.getElementById('folder').addEventListener(('click'), () => {
         addFolder();
     });
 
-    const removeFolder = () => {
-        for (let i = 0; i < folders.length; i++) {
-            document.querySelectorAll('.remove')[i].addEventListener('click', () => {
-                folders.splice(i, 1);
-                notes[i] = {};
-                showFolders(); 
-                removeFolder();
-            });
+
+    const createNote = (name) => {
+        return {
+            'name': name,
+            'description': '',
         }
     }
 
